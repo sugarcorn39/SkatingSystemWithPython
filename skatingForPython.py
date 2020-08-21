4699525297c8183e1f0299e21cb05e0c44d5cd99
sugarcorn <chihirogawa.com@gmail.com>
2020-08-21 22:36:46 +0900

    変数名やコメントなどを修正

--- skatingForPython.py
+++ skatingForPython.py
@@ -11,56 +11,64 @@ log = logger.Logger('skating')
 
 class Player:
     def __init__(self, _num, _name):
-        self.number = _num
-        self.name = _name
-        self.score = 0
-        self.order = []
-        self.majorityNum = 0
-        self.sumSuperiorNum = 0
-
-    #def setScore(self, ):
+        self.number = _num # 背番号
+        self.name = _name # 名前
+        self.score = 0 # 確定順位
+        self.order = [] # 審査結果
+        self.majorityNum = 0 # 多数決継続数
+        self.sumSuperiorNum = 0 # 上位加算結果
 
     def setOrder(self, _order):
         self.order.append(_order)
 
     def printPropaty(self):
-        print("number : " + str(self.number))
-        print("name   : " + str(self.name))
-        print("score  : " + str(self.score))
-        print("order  : " + str(self.order))
-        print("majorityNum  : " + str(self.majorityNum))
-        print("sumSuperiorNum  : " + str(self.sumSuperiorNum))
+        print("number         : " + str(self.number))
+        print("name           : " + str(self.name))
+        print("score          : " + str(self.score))
+        print("order          : " + str(self.order))
+        print("majorityNum    : " + str(self.majorityNum))
+        print("sumSuperiorNum : " + str(self.sumSuperiorNum))
 
-# ソート関数はsorted()を使用するだけなので不要
-
-
-# 同着
-def sameScore(_players, _deside_num):
-    log.debug("start sameScore")
+# 同着処理
+# 対象リストに対して、同着順位を付与する
+# 引数
+#   1, 対象リスト
+#   2, 決定順位
+def same_score(_players, _deside_num):
+    log.debug("start same_score")
 
     for player in _players:
         log.debug(str(player.number) + ":" + str(sorted(player.order)) )
 
     for player in _players:
-        print("同着により"+ str(_deside_num) +"を決定しました")
+        print("背番号" + str(player.number) + " : "+ str(_deside_num) +"位(同着)")
+
         player.score = _deside_num
+    
+    log.debug("end same_score")
+
 
 # 下位比較
-def compareSubordinate(_players, _deside_num):
-    log.debug("start compareSubordinate")
+# 対象リストに対して、下位比較を行い順位を付与する
+# 順位を確定できない場合、対象者を同着とする
+# 引数
+#   1, 対象リスト
+#   2, 決定順位
+def compare_subordinate(_players, _deside_num):
+    log.debug("start compare_subordinate")
 
     for player in _players:
         log.debug(str(player.number) + ":" + str(sorted(player.order)) )
 
-    # 無限ループ防止用
+    # 再帰処理終了
     if len(_players) <= 0:
-        log.debug("end compareSubordinate")
+        log.debug("end compare_subordinate")
         return
     elif len(_players) == 1:
-        print("下位比較により"+ str(_deside_num) +"を決定しました")
+        # 対象者が1人のため、順位を確定する
+        print("背番号" + str(_players[0].number) + " : "+ str(_deside_num) +"位(下位比較)")
         _players[0].score = _deside_num
 
-        # 対象者が1人のため、リターンする
         return
 
     center = int(len(_players[0].order)/2) # + 1
@@ -69,7 +77,6 @@ def compareSubordinate(_players, _deside_num):
     for player in _players:
         player.order.sort()
 
-        # 中央値から右側を比較していき、その時の数を計算する
         subordinate_num = []
         for i in range(center, len(player.order)-1):
             subordinate_num.append(player.order[i])
@@ -80,49 +87,49 @@ def compareSubordinate(_players, _deside_num):
 
     check_list = []
     for i in range(0,len(compareSubordinate_list)):
-        # print("～～～～～～～～～")
-        # print(i)
-        # print("compareSubordinate_list :" + str(compareSubordinate_list))
-        # print("compareSubordinate_list[i] :" + str(compareSubordinate_list[i]))
-        # print("min(compareSubordinate_list[i]) :" + str(min(compareSubordinate_list[i])))
-        # print("compareSubordinate_list[i].count(min(compareSubordinate_list[i])) :" + str(compareSubordinate_list[i].count(min(compareSubordinate_list[i]))))
-
-        # print("len(compareSubordinate_list) :" + str(len(compareSubordinate_list)))
-        # print("len(compareSubordinate_list[i]) :" + str(len(compareSubordinate_list[i])))
-        # print("～～～～～～～～～")
+        # 転置したリストに対して、
+        # 最小値の個数 と リストの長さ を比較し
+        # 異なった場合、最小値を持つ選手を抽出する
         if len(compareSubordinate_list[i]) != compareSubordinate_list[i].count(min(compareSubordinate_list[i])):
             check_list = [player for player in _players if player.order[center + i] == min(compareSubordinate_list[i])]
             break
         
+        # 対象者が完全一致の場合、
+        # 同着とする
         if i == len(compareSubordinate_list):
+            log.debug("下位比較 完全一致")
+
             check_list = _players
 
 
     if len(check_list) == 1:
         # 最大値が一つであれば、その値を持つ選手に順位を決定する
-        print("下位比較により"+ str(_deside_num) +"を決定しました")
+        print("背番号" + str(check_list[0].number) + " : "+ str(_deside_num) +"位(下位比較)")
         check_list[0].score = _deside_num
 
-    # 複数ある場合は、下位比較加算を行う
+    # 複数ある場合は、同着処理を行う
     elif  len(check_list) > 1:
         # 同着処理へ
-        sameScore(check_list, _deside_num)
+        same_score(check_list, _deside_num)
     else :
+        # TODO 不要か確認するためにログを出力する
+        log.debug("下位比較 check_listが0以下")
+
         check_list = _players
-        sameScore(check_list, _deside_num)
+        same_score(check_list, _deside_num)
 
     _players =  list( set(_players) ^ set(check_list))
     
-    # for player in _players:
-    #     player.printPropaty()
-
     # 次の順位を決定するため、再帰的に呼び出す
     _deside_num += len(check_list)
-
-    # print("次の順位"+ str(_deside_num) + "を決定する")
-    compareSubordinate(_players,_deside_num)
+    compare_subordinate(_players,_deside_num)
 
 # 上位加算
+# 対象リストに対して、上位加算を行い順位を付与する
+# 順位を確定できない場合、対象者に対して下位比較を行う
+# 引数
+#   1, 対象リスト
+#   2, 決定順位
 def total_superior(_players, _deside_num):
     log.debug("start total_superior")
 
@@ -159,39 +166,40 @@ def total_superior(_players, _deside_num):
     # 順位が確定する
     if len(check_list) == 1:
         # 最大値が一つであれば、その値を持つ選手に順位を決定する
-        print("上位加算により"+ str(_deside_num) +"を決定しました")
+        print("背番号" + str(check_list[0].number) + " : "+ str(_deside_num) +"位(上位加算)")
         check_list[0].score = _deside_num
         
     # 複数ある場合は、下位比較加算を行う
     else :
         # 下位比較へ
-        compareSubordinate(check_list, _deside_num)
+        compare_subordinate(check_list, _deside_num)
 
     # 対象差集合をset()を用いて取得する
     _players =  list( set(_players) ^ set(check_list))
 
     # 次の順位を決定するため、再帰的に呼び出す
     _deside_num += len(check_list)
-    # print("次の順位"+ str(_deside_num) + "を決定する")
     total_superior(_players,_deside_num)
 
 # 多数決
-# 中央値から右を確認し、一番長い選手に順位を覚醒させる
+# 対象リストに対して、多数決を行い順位を付与する
+# 順位を確定できない場合、対象者に対して上位加算を行う
 # 引数
-# １、対象の選手リスト
-# ２、決定順位
+#   1, 対象リスト
+#   2, 決定順位
 def majority(_players, _deside_num):
 
     log.debug("start majority")
     for player in _players:
         log.debug(str(player.number) + ":" + str(sorted(player.order)) )
 
-    # 無限ループ防止用
+    # 再帰処理終了
     if len(_players) <= 0:
         log.debug("end majority")
         return
 
-    # インデックスとして使用する場合は、+1 しない
+    # インデックスとして使用するため +1 しない
+    # TODO 審査員が偶数の場合に対応できるようにする
     center = int(len(_players[0].order)/2) # + 1
 
     for player in _players:
@@ -212,9 +220,6 @@ def majority(_players, _deside_num):
     majority_list = []
     for player in _players:
         majority_list.append(player.majorityNum)
-
-    # 不要では？
-    log.debug(str(majority_list))
     
     # 多数決による最大値が1件である場合、
     # 順位が確定する
@@ -224,7 +229,7 @@ def majority(_players, _deside_num):
 
     if len(check_list) == 1:
         # 最大値が一つであれば、その値を持つ選手に順位を決定する
-        print("多数決により"+ str(_deside_num) +"を決定しました")
+        print("背番号" + str(check_list[0].number) + " : "+ str(_deside_num) +"位(多数決)")
         check_list[0].score = _deside_num
 
     # 複数ある場合は、上位加算を行う
@@ -237,7 +242,6 @@ def majority(_players, _deside_num):
 
     # 次の順位を決定するため、再帰的に呼び出す
     _deside_num += len(check_list)
-    # print("次の順位"+ str(_deside_num) + "を決定する")
     majority(_players,_deside_num)
 
 
@@ -249,14 +253,18 @@ def majority(_players, _deside_num):
 # -> 直接入力
 
 # 過半数(中央値)
-# 再帰的に呼び出し処理を行う。
+# 対象リストに対して、過半数比較を行い順位を付与する
+# 順位を確定できない場合、対象者に対して多数決を行う
+# 引数
+#   1, 対象リスト
+#   2, 決定順位
 def median(_players, _deside_num):
 
     log.debug("start median")
     for player in _players:
         log.debug(str(player.number) + ":" + str(sorted(player.order)) )
 
-    # 無限ループ防止用
+    # 再帰処理終了
     if len(_players) <= 0:
         log.debug("end median")
         return
@@ -272,7 +280,7 @@ def median(_players, _deside_num):
                 check_list.append(player)
 
         if len(check_list) == 0:
-            # 対象者がいないため、抽出条件の順位を一つ下げる
+            # 対象者がいないため、抽出する順位を一つ下げる
             fetch_num += 1
         elif len(check_list) > 0:
             break
@@ -282,9 +290,7 @@ def median(_players, _deside_num):
         # 順位の付与
         # チェック対象者からの削除
         check_list[0].score = _deside_num
-        print("過半数により"+ str(_deside_num) +"を決定しました")
-        # print(check_list[0].name)
-        # print(check_list[0].score)
+        print("背番号" + str(check_list[0].number) + " : "+ str(_deside_num) +"位(過半数)")
 
     elif len(check_list) > 1:
         # 対象者に対して多数決の検証をする
@@ -295,11 +301,11 @@ def median(_players, _deside_num):
 
     # 次の順位を決定するため、再帰的に呼び出す
     _deside_num += len(check_list)
-    # print("次の順位"+ str(_deside_num) + "を決定する")
     median(_players,_deside_num)
 
 # スケーティング処理開始
 def skating(_player_list):
+    # 中央値比較を1位から順に決定する
     median(_player_list, 1)
 
 def main():
@@ -307,7 +313,7 @@ def main():
     # 仮の選手情報
     testPlayer1 = Player(10,"Tanaka")
     testPlayer2 = Player(20,"Sato")
-    testPlayer3 = Player(30,"Syzuki")
+    testPlayer3 = Player(30,"Suzuki")
     testPlayer4 = Player(40,"Morita")
     testPlayer5 = Player(50,"Oda")
     testPlayer6 = Player(60,"Naritake")
@@ -321,8 +327,11 @@ def main():
                    ]
 
     # ジャッジ数には奇数を設定する
-    judge_num = 151
-    # 入力の変わりにランダム値を入れる
+    # TODO 観客みんなで審査できるように大きな数を入れても問題ないようにする。
+    #      現在は1501で10秒ほどかかる
+    judge_num = 9
+
+    # 審査結果入力の変わりにランダム値を入れる
     order = list(range(1,len(player_list) + 1))
     rand_order = []
     for i in range(judge_num):
@@ -341,7 +350,7 @@ def main():
     # 結果出力
     print("スケーティング結果")
     for player in player_list:
-        print("number :" +  str(player.number) + " score:" +  str(player.score))
+        print("背番号" +  str(player.number) + " : " +  str(player.score) + "位")
 
 if __name__ == '__main__':
     main()
